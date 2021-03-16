37928df8ba5f68f074945c69d5a0658e6005368f
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-15 22:22:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+sen= [""];
+
+function pa1() {
+    for (let i = 0; i <=6  i++) {
+      sen.push(document.getElementById("p1input"+i).value;
+        
+    }
+    document.getElementById("pa1").innerHTML= sen.join("")
+}
+
+function pa2() {
+    for (let i = 0; i <=6  i++) {
+      sen.push(document.getElementById("p2input"+i).value;
+        
+    }
+    document.getElementById("pa2").innerHTML= sen.join(i)
+}
