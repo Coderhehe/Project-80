bbc1d5d03a78a23c17fc7bcfbf0bc735889d091e
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-31 11:17:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-sen= [""];
 
 function pa1() {
-    for (i = 0; i <=6  i++) {
-      sen.push(document.getElementById("p1input"+i).value;
+    sen=[];
+    for (i = 1; i <=5;i++) {
+      sen.push(document.getElementById("p1input"+i).value);
         
     }
-    document.getElementById("pa1").innerHTML= sen.join("")
+    document.getElementById("pa1").innerHTML= sen.join(" ");
 }
 
 function pa2() {
-    for (i = 0; i <=6  i++) {
-      sen.push(document.getElementById("p2input"+i).value;
+    sen=[];
+    for (i = 1; i <=5;i++) {
+      sen.push(document.getElementById("p2input"+i).value);
         
     }
-    document.getElementById("pa2").innerHTML= sen.join(i)
+    document.getElementById("pa2").innerHTML= sen.join(" ");
 }
